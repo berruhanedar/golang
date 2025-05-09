3e3515481bc2f8fd97bb379596b5128da6cc88ed
Berru Hanedar <berruhanedar@icloud.com>
2025-05-10 01:57:00 +0300

    Implement basic Go variables and print statements

---
+++ 02_variable_declaration/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var - name of variable - static type
+	var name string = "Berru"
+
+	var isim string
+	isim = "Burcu"
+
+	var age int
+	age = 24
+
+	var isStudent bool
+	isStudent = true
+
+	var firstName, lastName string = "Berru", "Hanedar"
+
+	ad := "Berru"
+
+	fmt.Println("Hello ", name)
+	fmt.Println("Hello ", isim)
+	fmt.Println(age)
+	fmt.Println(isStudent)
+	fmt.Println(firstName, lastName)
+	fmt.Println(ad)
+}
